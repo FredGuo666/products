5fd23c4fcf682ff73c2da6b02a1626d26fba2337
Fred <fredguo@Fred-Mac.local>
2022-01-17 11:37:47 +0800

    refactor

--- products.py
+++ products.py
@@ -1,43 +1,61 @@
 import os # operating system
 
 #读取档案
-#strip 除掉换行符(/n)
-products = []
-if os.path.isfile('products.csv'): # 检查文件是否存在
-	print('找到文件!')
-	with open('products.csv', 'r', encoding='utf-8') as f:
+def read_file(filename):
+	products = []
+	with open(filename, 'r', encoding='utf-8') as f:
 		for line in f:
 			if '商品,价格' in line:
 				continue  # 跳到下一个回圈
 			name, price = line.strip().split(',')
 			products.append([name, price])
-	print(products)		
-else: 
-	print('找不到文件....')
+	return(products)
 
 # 请使用者输入
-while True:
-	name = input('请输入商品名称:')
-	if name == 'q':
-		break
-	price = input('请输入商品价格:')
-	price = int(price)
-	# p = []
-	# p.append(name)
-	# p.append(price)
-	# p = [name, price]
-	products.append([name, price])
-print(products)
+def user_input(products):
+	while True:
+		name = input('请输入商品名称:')
+		if name == 'q':
+			break
+		price = input('请输入商品价格:')
+		price = int(price)
+		# p = []
+		# p.append(name)
+		# p.append(price)
+		# p = [name, price]
+		products.append([name, price])
+	print(products)
+	return products
 
 # 印出所有购买记录
-for p in products:
-	print(p[0], '的价格是', p[1])
-
-# 'abc' + '123' = 'abc123'
-# 'abc' * 3 = 'abcabcabc'
+def print_products(products):
+	for p in products:
+		print(p[0], '的价格是', p[1])
 
 # 写入档案
-with open('products.csv', 'w', encoding='utf-8') as f:    # with 自动close
-	f.write('商品,价格\n')
-	for p in products:
-		f.write(p[0] + ',' + str(p[1]) + '\n')   # ','换格， '\n'换行
+def write_file(filename, products):
+	with open(filename, 'w', encoding='utf-8') as f:    # with 自动close
+		f.write('商品,价格\n')
+		for p in products:
+			f.write(p[0] + ',' + str(p[1]) + '\n')   # ','换格， '\n'换行
+
+
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename): # 检查文件是否存在
+		print('找到文件!')
+		products = read_file(filename)
+	else:
+		print('找不到文件')
+
+	products = user_input(products)  
+	print_products(products)
+	write_file('products.csv', products)
+
+main()
+
+# 理想的function应该只做一件事。
+# refactor 重构
+
+
+
